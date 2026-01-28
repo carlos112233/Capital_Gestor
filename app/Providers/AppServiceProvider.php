c441fa09dc500727cc1cb9b8b074a1527a23ae62
desarrollo6 <desarrollo6@xpd.mx>
2026-01-28 10:58:36 -0600

    Truco de migración de emergencia

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Artisan; // <-- Agrega esta línea
+use Illuminate\Support\Facades\Schema; 
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (isset($_GET['comando_secreto'])) {
+            Artisan::call('migrate', ['--force' => true]);
+            dump("Migración ejecutada con éxito");
+        }
     }
 }
