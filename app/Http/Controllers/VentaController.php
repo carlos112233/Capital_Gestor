5fc41d23716b446c5850d75755ebe446ae8784d5
desarrollo6 <desarrollo6@xpd.mx>
2026-01-28 15:51:17 -0600

    Fix Notificaciones2

--- app/Http/Controllers/VentaController.php
+++ app/Http/Controllers/VentaController.php
@@ -93,7 +93,7 @@ class VentaController extends Controller
                     ]);
 
                     Notification::route('mail', 'ander.234.cm@gmail.com')
-                        ->notify(new \App\Notifications\NuevoPedidoNotification($pedido));
+                        ->notify(new NuevoPedidoNotification($pedido));
                 }
             });
         } catch (\Illuminate\Validation\ValidationException $e) {
