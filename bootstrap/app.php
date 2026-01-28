a7addd81269ee15fd5eaab78693eec3555b38f26
desarrollo6 <desarrollo6@xpd.mx>
2026-01-28 13:56:49 -0600

    Fix: AuthLogin3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,10 +10,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-     $middleware->alias(['role'=>RoleMiddleware::class]);
+    ->withMiddleware(function (Middleware $middleware) {
+        // AÑADE ESTA LÍNEA PARA RENDER
+        $middleware->trustProxies(at: '*');
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
-
