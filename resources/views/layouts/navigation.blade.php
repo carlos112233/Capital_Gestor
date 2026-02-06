92a466e5810e9bf1ce39a98eb97f9b63abb41476
desarrollo6 <desarrollo6@xpd.mx>
2026-02-06 11:46:02 -0600

    nac

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -23,12 +23,12 @@
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     @if (Auth::user()->hasRole('admin'))
                         <x-nav-link :href="route('dashboardAdmin')" :active="request()->routeIs('dashboardAdmin')">
-                            {{ __('dashboard') }}
+                            {{ __('Dashboard') }}
                         </x-nav-link>
                     @endif
                     @if (Auth::user()->hasRole('user'))
                         <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                            {{ __('dashboard') }}
+                            {{ __('Dashboard') }}
                         </x-nav-link>
                     @endif
 
