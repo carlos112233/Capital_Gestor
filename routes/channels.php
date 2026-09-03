ed86f6a3882674c81ea5c293eb9fb4163b9b17f0
desarrollo6 <desarrollo6@xpd.mx>
2026-09-03 16:31:39 -0600

    Add User channel authorization for real-time notifications

--- routes/channels.php
+++ routes/channels.php
@@ -6,3 +6,7 @@ Broadcast::channel('pedidos', function ($user) {
     // Validamos que solo admin pueda escuchar
     return $user->hasRole('admin');
 });
+
+Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});
